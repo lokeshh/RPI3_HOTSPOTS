318a0f56b45dc2694c7b2365c6a3ee5fbfc9229b
quangthanh010290 <thanh@autonomous.nyc>
2016-12-21 13:54:24 +0700

    clean ipaddress before connect

--- sta.sh
+++ sta.sh
@@ -6,6 +6,7 @@ sudo sed -i -e "s/\(psk=\).*/\1$b/" /etc/wpa_supplicant/wpa_supplicant.conf
 sudo /etc/init.d/hostapd stop
 sudo /etc/init.d/udhcpd stop
 sudo ifdown wlan0
+sudo ip addr flush dev wlan0
 sudo ifconfig wlan0 down
 sudo rm -rf /etc/network/interfaces
 sudo cp /etc/network/interfaces.sta /etc/network/interfaces
