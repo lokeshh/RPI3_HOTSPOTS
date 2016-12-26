b69d2d17b2b191a3e53a12cc26d9c813de305efe
quangthanh010290 <thanh@autonomous.nyc>
2016-12-26 17:00:15 +0700

    Update ap.sh

--- ap.sh
+++ ap.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
 if [ -z "$1" ] 
 then
-  echo "Please Input Access Point Name"
-  exit(1)
+  echo "No Input Name, select default name:ThanhLe_handsome"
+  name="ThanhLe_handsome"
+  sudo sed -i -e "s/\(ssid=\).*/\1$name/" /etc/hostapd/hostapd.conf
+  
 else
   sudo sed -i -e "s/\(ssid=\).*/\1$1/" /etc/hostapd/hostapd.conf
 fi
-if [ -z "$2" ] 
+if [ -z  "$2" ] 
 then
   echo "No Pass Hotspot"
   x=0
   sudo sed -i -e "s/\(wpa=\).*/\1$x/" /etc/hostapd/hostapd.conf
-  exit(1)
 else
   x=2
   sudo sed -i -e "s/\(wpa=\).*/\1$x/" /etc/hostapd/hostapd.conf
