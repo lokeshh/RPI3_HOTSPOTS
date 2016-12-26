298c186b752e399c355c101fb20282227f4d65b1
quangthanh010290 <thanh@autonomous.nyc>
2016-12-26 09:33:01 +0000

    update

--- sta.sh
+++ sta.sh
@@ -3,12 +3,13 @@ a="\"$1\""
 b="\"$2\""
 sudo sed -i -e "s/\(ssid=\).*/\1$a/" /etc/wpa_supplicant/wpa_supplicant.conf 
 sudo sed -i -e "s/\(psk=\).*/\1$b/" /etc/wpa_supplicant/wpa_supplicant.conf
-sudo /etc/init.d/hostapd stop
-sudo /etc/init.d/udhcpd stop
-sudo ifdown wlan0
-sudo ip addr flush dev wlan0
-sudo ifconfig wlan0 down
+sudo /etc/init.d/hostapd stop &&
+sudo /etc/init.d/udhcpd stop &&
+sudo ifdown wlan0 &&
+sudo ip addr flush dev wlan0 &&
+sudo ifconfig wlan0 down &&
 sudo rm -rf /etc/network/interfaces
 sudo cp /etc/network/interfaces.sta /etc/network/interfaces
-sudo ifconfig wlan0 up
+sudo ifconfig wlan0 up &&
+sudo service dhcpcd try-restart &&
 sudo ifup wlan0
