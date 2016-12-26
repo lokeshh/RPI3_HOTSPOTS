ae7e1a6408f1aac55e933862cdfd7cd589cf3732
quangthanh010290 <thanh@autonomous.nyc>
2016-12-26 11:12:10 +0000

    update

--- sta.sh
+++ sta.sh
@@ -3,6 +3,8 @@
 if [ -z "$1" ] && [ -z "$2" ] 
 then
   echo "Connect using current information"
+  sudo rm -rf /etc/network/interfaces
+  sudo cp /etc/network/interfaces.sta /etc/network/interfaces
 else
   a="\"$1\""
   b="\"$2\""
@@ -11,7 +13,7 @@ else
 fi
 
 sudo /etc/init.d/hostapd stop &&
-sudo /etc/init.d/udhcpd stop &&
+#sudo /etc/init.d/udhcpd stop &&
 sudo ifdown wlan0 &&
 sudo ip addr flush dev wlan0 &&
 sudo ifconfig wlan0 down &&
